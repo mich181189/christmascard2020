e5fe47d5e4c51455241fe2cf7dcc159023bd3aac
Michael Cullen <michael@smokeemittingdiode.co.uk>
2020-12-17 18:51:51 +0000

    More docs tweaks

--- docs/examples/default_main.py
+++ docs/examples/default_main.py
@@ -1,8 +1,8 @@
 import ws2812
 import utime
-import random
 from lm75 import LM75
 from machine import I2C
+import random
 
 i2c = I2C(1, freq=400000)
 lm75 = LM75(i2c)
@@ -71,6 +71,62 @@ def pattern_twinkle(starting):
 		utime.sleep_ms(100)
 		twinkle_next(state)
 
+def pattern_fade(starting_leds):
+	class LEDState:
+		STATE_WAIT = 0
+		STATE_RAMP_UP = 1
+		STATE_RAMP_DOWN = 2
+
+		def __init__(self, i, r, g, b):
+			self.i = i
+			self.wait_count = random.randint(0, 35)
+			self.base_r = r
+			self.base_g = g
+			self.base_b = b
+			self.current_state = 0
+			self.step = 0
+
+		def run(self):
+			if self.current_state == self.STATE_RAMP_UP:
+				self.step += 1
+				if self.step >= 7:
+					self.current_state = self.STATE_RAMP_DOWN
+			elif self.current_state == self.STATE_RAMP_DOWN:
+				self.step -= 1
+				if self.step <= 0:
+					self.current_state = self.STATE_WAIT
+					self.wait_count = 0
+			else:
+				# the aim of thisis to make something that looks sort of chaotic
+				if self.wait_count < (75 - 2*(self.i % 3) + (self.i % 2) - (self.i % 4)) - self.i:
+					self.wait_count += 1
+					#ws2812.set_led(self.i, 0,0,0)
+				else:
+					self.current_state = self.STATE_RAMP_UP
+			r = 0
+			g = 0
+			b = 0
+			if self.base_r > 0:
+				r = self.base_r	 + self.step*2 - 5
+			if self.base_g > 0:
+				g = self.base_g + self.step*2 - 5
+			if self.base_b > 0:
+				b = self.base_b + self.step*2 - 5
+			ws2812.set_led(self.i, int(r), int(g), int(b))
+
+	leds = []
+	i = 0
+	for l in starting_leds:
+		r,g,b = l
+		leds.append(LEDState(i, r, g, b))
+		i += 1
+
+	while True:
+		for l in leds:
+			l.run()
+		utime.sleep_ms(50)
+
+
 def pattern1():
 	leds = [
 		(10,10,0),
@@ -82,7 +138,10 @@ def pattern1():
 		(0,10,0),(0,10,0),(0,10,0),(0,10,0),(0,10,0),
 		(10,0,0),(0,10,0),(0,10,0),(0,10,0),(0,10,0),(10,0,0),
 		]
-	pattern_twinkle(leds)
+	if int(start_temperature * 100) % 2 == 0:
+		pattern_twinkle(leds)
+	else:
+		pattern_fade(leds)
 
 def pattern2():
 	leds = [
@@ -95,7 +154,10 @@ def pattern2():
 		(0,0,10),(0,0,10),(0,0,10),(0,0,10),(0,0,10),
 		(10,0,10),(0,0,10),(0,0,10),(0,0,10),(0,0,10),(10,0,10),
 		]
-	pattern_twinkle(leds)
+	if int(start_temperature * 100) % 2 == 0:
+		pattern_twinkle(leds)
+	else:
+		pattern_fade(leds)
 
 def pattern3():
 	leds = [
@@ -108,15 +170,26 @@ def pattern3():
 		(10,0,0),(10,0,0),(10,0,0),(10,0,0),(10,0,0),
 		(0,10,5),(10,0,0),(10,0,0),(10,0,0),(10,0,0),(0,10,5),
 		]
-	pattern_twinkle(leds)
+	if int(start_temperature * 100) % 2 == 0:
+		pattern_twinkle(leds)
+	else:
+		pattern_fade(leds)
 
 def pattern4():
-	import urandom
 	while True:
 		for i in range(28):
-			ws2812.set_led(i, urandom.randint(0,15),urandom.randint(0,15),urandom.randint(0,15))
-		utime.sleep_ms(200)
+			ws2812.set_led(i, random.randint(0,15),random.randint(0,15),random.randint(0,15))
+		utime.sleep_ms(300)
 
+random.seed()
 
-print("Starting temperature was " + str(start_temperature))
-pattern4()
+print("Starting temperature is %f" % (start_temperature,))
+
+if start_temperature < 20:
+	pattern2()
+elif start_temperature < 25:
+	pattern1()
+elif start_temperature < 26:
+	pattern4()
+else:
+	pattern3()
